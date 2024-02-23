2ddfd11d67d866545c542fd2ee9494696cb444c2
Sinsa9706 <ox63023@gmail.com>
2024-02-23 19:42:04 +0900

    feat 인벤토리 기능, 아이템 장착 및 사용 기능

--- Survival/Assets/Scriptable Objects/Scripts/ItemData.cs
+++ Survival/Assets/Scriptable Objects/Scripts/ItemData.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +16,13 @@ public enum ConsumableType
     Health
 }
 
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
 [CreateAssetMenu(fileName = "Item", menuName = "New Item")]
 public class ItemData : ScriptableObject
 {
@@ -29,5 +37,10 @@ public class ItemData : ScriptableObject
     public bool canStack;
     public int maxStackAmount;
 
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
 
+
+    [Header("Equip")]
+    public GameObject equipPrefab;
 }
